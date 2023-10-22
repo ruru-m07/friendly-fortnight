37ef2c6cffccd8f24ed249541a10bc80147e5a94
ruru-m07 <ruru.dev07@gmail.com>
2023-10-22 14:54:24 +0530

    ✅ complete  8 routes  of user

---
+++ server/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+const mongoURI = process.env.MONGODB_URI;
+
+const connectToMongo = async () => {
+  mongoose.connect(mongoURI, () => {
+    console.log("Connected to Mongo Successfully");
+  });
+};
+
+module.exports = connectToMongo;
---
+++ server/index.js
@@ -0,0 +1,22 @@
+const connectToMongo = require("./db");
+var cors = require("cors");
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const UserRoutes = require('./routes/UserRoutes')
+
+// ** connect to mongoDB server
+connectToMongo();
+
+const port = process.env.PORT;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/user", UserRoutes);
+
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
